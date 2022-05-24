42c933d88bcac233e82d832d6c354c9572a2d4f0
John <johnaguiar.dev@gmail.com>
2022-05-24 11:13:12 -0300

    add extra files to finish the project (before structuring in components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import animelogo from './images/animesz-logo.png'
+import profilelogo from "./images/profile-user.png"
+import searchLogo from './images/search-icon.png'
+import githubLogo from './images/github-logo.png'
 
 function App() {
-  const [url, setUrl] = useState('https://api.aniapi.com/v1/anime');
+  const [url, setUrl] = useState('https://api.aniapi.com/v1/anime?&page=1&per_page=50');
   const [animeList, setAnimeList] = useState([])
+  const [sideAnimeList, setSideAnimeList] = useState([])
 
   const loadAnimes = async () => {
     let response = await fetch(url);
@@ -11,8 +16,15 @@ function App() {
     setAnimeList(json.data.documents)
 // Retorna um array de objetos
   }
+
+  const loadSideAnimes = async ()=> {
+    let response = await fetch('https://api.aniapi.com/v1/anime?&page=2&per_page=24')
+    let json = await response.json();
+    setSideAnimeList(json.data.documents)
+  }
   useEffect(()=>{
     loadAnimes()
+    loadSideAnimes()
   },[])
 
   function formatTitle(string){
@@ -32,22 +44,36 @@ function App() {
     <div className="App"> 
 
       <header> 
-      <div className="logo-brand">logo do site</div>
-      <nav>
-        <ul>
-          <li>Random</li>
-          <li>Animes</li>
-          <li>Filmes</li>
-          <li>Gêneros</li>
-        </ul>
-      </nav>
-      <input type="text" placeholder='Pesquisar...'/>
-      <div>Image-login</div>
+      <div className="header-content">
 
+        <div className="header-left-side">  
+          <div>
+          <div className="logo-container"> <img className="logo-brand" src={animelogo} alt="" /> </div>
+          </div>
+          <nav>
+            <ul>
+              <li>Random</li>
+              <li>Animes</li>
+              <li>Filmes</li>
+              <li>Gêneros</li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="header-right-side">
+          <div className='search-input'>
+           <input  type="text" placeholder='Pesquisar...'/>
+           <img src={searchLogo} alt="search logo" />
+          </div>
+         
+          <img id="profile-logo" src={profilelogo} alt="" />
+        </div>
+
+      </div>
       </header>
 
+      <main>
       <div className="animelist-container">
-
         {animeList.map((item, key)=>(
           <div key={key} className="anime-card">
             <img className="cover-img"src={item.cover_image} alt={item.titles.en} />
@@ -55,9 +81,21 @@ function App() {
           </div>
         ))}
         </div>
-      <div className="side-animes">
-        Lista de animes aqui
-      </div>
+        <div className="side-animes">
+          Em alta
+          {sideAnimeList.map((item, index)=>(
+              <div key={index} className="anime-side-card">
+                <img className='banner-img' src={item.banner_image} alt={item.titles.en} />
+                <span className='anime-side-title-absolute'>{item.titles.en}</span>
+              </div>
+          ))}
+        </div>
+     </main>
+     <footer>
+       <span> Developed by <a href="https://github.com/john-aguiar"> <img src={githubLogo} className="github-logo" alt="" /> john-aguiar </a>  © 2022</span>
+       <span>Layout inspired by <a href="https://animeshouse.net/">animeshouse.net</a></span>
+     </footer>
+
     </div>
   )
 }
